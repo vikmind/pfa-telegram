3db930e01a77fac8d9c990d7363d439abe507d5a
Viktor Marchenko <viktor.marchenko@gmail.com>
2017-09-19 00:27:26 +0300

    Init

---
+++ README.md
@@ -0,0 +1,3 @@
+# PFA Telegram
+
+Telegram client for PFA app. Initial version should support only stylesheet workflow.
---
+++ index.js
@@ -0,0 +1,30 @@
+const Telegraf = require('telegraf')
+
+const app = new Telegraf(process.env.BOT_TOKEN)
+const state = {}
+app.command('start', ({ from, reply }) => {
+  console.log('start', from)
+  return reply(`Welcome ${from.username}!`)
+})
+app.command('add', ({ from, reply }) => {
+  state[from.username] = { listen: true, data: [] };
+  return reply('I\'m listening, format is:\nSUM CATEGORY; <DESCRIPTION> | <DATE>');
+});
+app.command('end', ({ from, reply }) => {
+  state[from.username] = { listen: false };
+  // Send data
+  return reply('End of input');
+});
+app.hears(/([\d\+]+)\s([\w\s]+)(?:(?:;\s)([\w\s]*))?(?:(?:\s?\|\s?)(.*))?/, ({ match, from, reply }) => {
+  if (!!state[from.username]) {
+    state[from.username].data.push({
+      amount: match[1],
+      category: match[2],
+      description: match[3],
+      date: match[4] || (new Date).toJSON().slice(0, 10),
+    });
+    return reply(JSON.stringify(state[from.username].data, null, 2));
+  }
+});
+console.log('start listening');
+app.startPolling()
