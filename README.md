64afa99977befbea96b595b8c387d6b8e8671d85
Viktor Marchenko <viktor.marchenko@gmail.com>
2017-09-24 20:50:39 +0300

    Add GAPI example, replace yarn with npm5

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
-# PFA Telegram
+# PFA Telegram bot
 
-Telegram client for PFA app. Initial version should support only stylesheet workflow.
+Send your spendings to bot and it will add them to specified stylesheet.
+
+## Quick start
+
+1. Add google credentials as described in [Google Sheets API Node.js Quickstart](https://developers.google.com/sheets/api/quickstart/nodejs#step_1_turn_on_the_api_name) and place ``client_secret.json`` in working dir.
+1. Create your bot with [@botfather](https://telegram.me/botfather)
+1. Create stylesheet with template ``Monthly budget``
+1. ``cp .env.example .env``
+1. Fill ``.env`` with bot key and your stylesheet ID
+
+## TODO
+
+1. Store credentials in mongo
+1. Move to custom backend
--- index.js
+++ index.js
@@ -1,30 +1,42 @@
-const Telegraf = require('telegraf')
 require('dotenv-safe').load()
+const Telegraf = require('telegraf')
+const gapi = require('./gapi');
+const { Extra } = require('Telegraf');
 
 const app = new Telegraf(process.env.BOT_TOKEN)
-const state = {}
+const markup = Extra.HTML();
+
 app.command('start', ({ from, reply }) => {
-  console.log('start', from)
-  return reply(`Welcome ${from.username}!`)
-})
-app.command('add', ({ from, reply }) => {
-  state[from.username] = { listen: true, data: [] };
-  return reply('I\'m listening, format is:\nSUM CATEGORY; <DESCRIPTION> | <DATE>');
+  const link = gapi.getLink(from.username);
+  return reply(`Welcome ${from.username}! Send code from this <a href="${link}">link</a> for authorization.`);
 });
-app.command('end', ({ from, reply }) => {
-  state[from.username] = { listen: false };
-  // Send data
-  return reply('End of input');
+app.command('auth', ({ from, reply }) => {
+  const link = gapi.getLink(from.username);
+  return reply(`Send code from this <a href="${link}">link</a> for authorization.`);
+});
+app.command('help', ({ reply }) =>
+  reply('<b>Message format</b>:\nSUM CATEGORY; &lt;DESCRIPTION&gt; | &lt;DATE&gt;', markup)
+);
+app.hears(/(.*)/, ({ match, from, reply }) => {
+  if (!gapi.isAuthorized(from.username)) {
+    return gapi.authorize(from.username, match[1])
+      .then(() => reply('You successfully authorized, run \/help for available commands'))
+      .catch(e => reply(`Can't authorize you, because: <b>${e.message}</b>`, markup))
+  }
 });
 app.hears(/([\d\+\-]+)\s([\w\s]+)(?:(?:;\s)([\w\s]*))?(?:(?:\s?\|\s?)(.*))?/, ({ match, from, reply }) => {
-  if (!!state[from.username]) {
-    state[from.username].data.push({
+  if (gapi.isAuthorized(from.username)) {
+    const data = {
       amount: match[1],
       category: match[2],
       description: match[3],
       date: match[4] || (new Date).toJSON().slice(0, 10),
-    });
-    return reply(JSON.stringify(state[from.username].data, null, 2));
+    };
+    return gapi.append(from.username, data)
+      .then(() => reply('Done!'))
+      .catch(e => reply(`Can't do this, because: <b>${e.message}</b>`, markup))
+  } else {
+    // Reauthorize
   }
 });
 console.log('start listening');
