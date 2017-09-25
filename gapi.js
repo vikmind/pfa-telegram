1bc2a0f1d2b6ab175ef931d60e07094f0988f218
Viktor Marchenko <viktor.marchenko@gmail.com>
2017-09-25 23:42:56 +0300

    Complete basic version for "Monthly budget" template

--- gapi.js
+++ gapi.js
@@ -1,89 +1,40 @@
-var fs = require('fs');
-var readline = require('readline');
-var google = require('googleapis');
-var googleAuth = require('google-auth-library');
+const fs = require('fs');
+const google = require('googleapis');
+const googleAuth = require('google-auth-library');
+const secrets = require('./client_secret.json');
 
-// If modifying these scopes, delete your previously saved credentials
-// at ~/.credentials/sheets.googleapis.com-nodejs-quickstart.json
-var SCOPES = ['https://www.googleapis.com/auth/spreadsheets'];
-var TOKEN_DIR = (process.env.HOME || process.env.HOMEPATH ||
+const SCOPES = ['https://www.googleapis.com/auth/spreadsheets'];
+const TOKEN_DIR = (process.env.HOME || process.env.HOMEPATH ||
     process.env.USERPROFILE) + '/.credentials/';
-var TOKEN_PATH = TOKEN_DIR + 'sheets.googleapis.com-nodejs-quickstart.json';
+const getCredentialsPath =
+  username => `${TOKEN_DIR}sheets.googleapis.com-pfa-telgram-${username}.json`;
 
-// Load client secrets from a local file.
-fs.readFile('client_secret.json', function processClientSecrets(err, content) {
-  if (err) {
-    console.log('Error loading client secret file: ' + err);
-    return;
-  }
-  // Authorize a client with the loaded credentials, then call the
-  // Google Sheets API.
-  // authorize(JSON.parse(content), listMajors);
-});
+const store = {};
 
-/**
- * Create an OAuth2 client with the given credentials, and then execute the
- * given callback function.
- *
- * @param {Object} credentials The authorization client credentials.
- * @param {function} callback The callback to call with the authorized client.
- */
-function authorize(credentials, callback) {
-  var clientSecret = credentials.installed.client_secret;
-  var clientId = credentials.installed.client_id;
-  var redirectUrl = credentials.installed.redirect_uris[0];
-  var auth = new googleAuth();
-  var oauth2Client = new auth.OAuth2(clientId, clientSecret, redirectUrl);
-
-  // Check if we have previously stored a token.
-  fs.readFile(TOKEN_PATH, function(err, token) {
-    if (err) {
-      getNewToken(oauth2Client, callback);
+function getClient(credentials, clients, username) {
+  return new Promise((resolve, reject) => {
+    if (!!clients[username]) {
+      resolve(clients[username])
     } else {
-      oauth2Client.credentials = JSON.parse(token);
-      callback(oauth2Client);
+      const clientSecret = credentials.installed.client_secret;
+      const clientId = credentials.installed.client_id;
+      const redirectUrl = credentials.installed.redirect_uris[0];
+      const auth = new googleAuth();
+      const oauth2Client = new auth.OAuth2(clientId, clientSecret, redirectUrl);
+      clients[username] = oauth2Client;
+      fs.readFile(getCredentialsPath(username), function(err, token) {
+        if (err) {
+          resolve(oauth2Client);
+        } else {
+          oauth2Client.credentials = JSON.parse(token);
+          resolve(oauth2Client);
+        }
+      });
     }
   });
 }
 
-/**
- * Get and store new token after prompting for user authorization, and then
- * execute the given callback with the authorized OAuth2 client.
- *
- * @param {google.auth.OAuth2} oauth2Client The OAuth2 client to get token for.
- * @param {getEventsCallback} callback The callback to call with the authorized
- *     client.
- */
-function getNewToken(oauth2Client, callback) {
-  var authUrl = oauth2Client.generateAuthUrl({
-    access_type: 'offline',
-    scope: SCOPES
-  });
-  console.log('Authorize this app by visiting this url: ', authUrl);
-  var rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-  });
-  rl.question('Enter the code from that page here: ', function(code) {
-    rl.close();
-    oauth2Client.getToken(code, function(err, token) {
-      if (err) {
-        console.log('Error while trying to retrieve access token', err);
-        return;
-      }
-      oauth2Client.credentials = token;
-      storeToken(token);
-      callback(oauth2Client);
-    });
-  });
-}
-
-/**
- * Store token to disk be used in later program executions.
- *
- * @param {Object} token The token to store to disk.
- */
-function storeToken(token) {
+function storeToken(token, username) {
   try {
     fs.mkdirSync(TOKEN_DIR);
   } catch (err) {
@@ -91,53 +42,61 @@ function storeToken(token) {
       throw err;
     }
   }
-  fs.writeFile(TOKEN_PATH, JSON.stringify(token));
-  console.log('Token stored to ' + TOKEN_PATH);
+  fs.writeFile(getCredentialsPath(username), JSON.stringify(token));
 }
 
-/**
- * Print the names and majors of students in a sample spreadsheet:
- * https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
- */
-function listMajors(auth) {
-  var sheets = google.sheets('v4');
-  sheets.spreadsheets.values.get({
-    auth: auth,
-    spreadsheetId: '1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms',
-    range: 'Class Data!A2:E',
-  }, function(err, response) {
-    if (err) {
-      console.log('The API returned an error: ' + err);
-      return;
-    }
-    var rows = response.values;
-    if (rows.length == 0) {
-      console.log('No data found.');
-    } else {
-      console.log('Name, Major:');
-      for (var i = 0; i < rows.length; i++) {
-        var row = rows[i];
-        // Print columns A and E, which correspond to indices 0 and 4.
-        console.log('%s, %s', row[0], row[4]);
-      }
-    }
-  });
-}
+const convertDataToRow = record =>
+  [
+    !!record.date ? record.date.toString() : '',
+    !!record.amount ? record.amount.toString() : '',
+    !!record.description ? record.description.toString() : '',
+    !!record.category ? record.category.toString() : ''
+  ];
 
 module.exports = {
-  isAuthorized: function(username) {
-    return username === 'vikmind';
-  },
-  getLink: function(username) {
+  getClient: getClient.bind(null, secrets, store),
+  getLink: function(oauth2Client) {
     return oauth2Client.generateAuthUrl({
       access_type: 'offline',
       scope: SCOPES
     });
   },
-  authorize: function(username, token) {
-    return Promise.resolve(true);
+  authorize: function(oauth2Client, username, code) {
+    return new Promise((resolve, reject)=> {
+      if (Object.keys(oauth2Client.credentials).length !== 0) {
+        resolve('already');
+      } else {
+        oauth2Client.getToken(code, function(err, token) {
+          if (err) {
+            console.log('Error while trying to retrieve access token', err);
+            return reject(err);
+          }
+          oauth2Client.credentials = token;
+          storeToken(token, username);
+          resolve(token);
+        }); 
+      }
+    });
   },
-  append: function(username, data) {
-
+  append: function(auth, data) {
+    return new Promise((resolve, reject) => {
+      const sheets = google.sheets('v4');
+      sheets.spreadsheets.values.append({
+        auth: auth,
+        spreadsheetId: process.env.SHEET_ID,
+        range: 'Transactions!B6:E',
+        valueInputOption: 'USER_ENTERED',
+        resource: {
+          values: [convertDataToRow(data)]
+        }
+      }, function(err, response) {
+        if (err) {
+          console.log('The API returned an error: ' + err);
+          return reject(err);
+        }
+        console.log(response);
+        resolve('OK');
+      })
+    });
   },
 };
--- index.js
+++ index.js
@@ -6,38 +6,37 @@ const { Extra } = require('Telegraf');
 const app = new Telegraf(process.env.BOT_TOKEN)
 const markup = Extra.HTML();
 
-app.command('start', ({ from, reply }) => {
-  const link = gapi.getLink(from.username);
-  return reply(`Welcome ${from.username}! Send code from this <a href="${link}">link</a> for authorization.`);
-});
-app.command('auth', ({ from, reply }) => {
-  const link = gapi.getLink(from.username);
-  return reply(`Send code from this <a href="${link}">link</a> for authorization.`);
-});
+app.command('start', ({ from, reply }) =>
+  gapi.getClient(from.username)
+    .then(client => 
+      reply(`Welcome ${from.username}! Send code from this <a href="${gapi.getLink(client)}">link</a> for authorization.`, markup))
+);
+app.command('auth', ({ from, reply }) =>
+  gapi.getClient(from.username)
+    .then(client => 
+      reply(`Send code from this <a href="${gapi.getLink(client)}">link</a> for authorization.`, markup))
+);
 app.command('help', ({ reply }) =>
   reply('<b>Message format</b>:\nSUM CATEGORY; &lt;DESCRIPTION&gt; | &lt;DATE&gt;', markup)
 );
-app.hears(/(.*)/, ({ match, from, reply }) => {
-  if (!gapi.isAuthorized(from.username)) {
-    return gapi.authorize(from.username, match[1])
-      .then(() => reply('You successfully authorized, run \/help for available commands'))
-      .catch(e => reply(`Can't authorize you, because: <b>${e.message}</b>`, markup))
-  }
-});
-app.hears(/([\d\+\-]+)\s([\w\s]+)(?:(?:;\s)([\w\s]*))?(?:(?:\s?\|\s?)(.*))?/, ({ match, from, reply }) => {
-  if (gapi.isAuthorized(from.username)) {
-    const data = {
+app.hears(/([\d\+\-]+)\s([\w\s]+)(?:(?:;\s)([\w\s]*))?(?:(?:\s?\|\s?)(.*))?/, ({ match, from, reply }) => 
+  gapi.getClient(from.username)
+    .then(client => gapi.append(client, {
       amount: match[1],
       category: match[2],
       description: match[3],
       date: match[4] || (new Date).toJSON().slice(0, 10),
-    };
-    return gapi.append(from.username, data)
-      .then(() => reply('Done!'))
-      .catch(e => reply(`Can't do this, because: <b>${e.message}</b>`, markup))
-  } else {
-    // Reauthorize
-  }
-});
+    }))
+    .then(msg => reply('Done!'))
+    .catch(e => reply(`Can't do this, because: <b>${e.message}</b>`, markup))
+);
+app.hears(/(.*)/, ({ match, from, reply }) =>
+  gapi.getClient(from.username)
+    .then(client => gapi.authorize(client, from.username, match[1]))
+    .then(status => (status !== 'already')
+        ? reply('You successfully authorized, run \/help for available commands')
+        : null)
+    .catch(e => reply(`Can't authorize you, because: <b>${e.message}</b>`, markup))
+);
 console.log('start listening');
 app.startPolling()
