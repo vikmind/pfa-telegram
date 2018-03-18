27d974dfcc5c7d200a26b9bb0db9276531df69aa
Viktor Marchenko <viktor.marchenko@gmail.com>
2018-03-18 23:49:03 +0300

    Buttons for categories

--- createApp.js
+++ createApp.js
@@ -7,6 +7,7 @@ module.exports = function({
   const app = new Telegraf(token)
   const markup = Telegraf.Extra.HTML();
   const path = 'pfa-bot-webhook-listener';
+  const Markup = Telegraf.Markup;
 
   app.command('start', ({ from, reply }) =>
     gapi.getClient(from.username)
@@ -14,32 +15,48 @@ module.exports = function({
         reply(`Welcome ${from.username}! Send code from this <a href="${gapi.getLink(client)}">link</a> for authorization.`, markup))
       .catch(e => reply(`Can't do this, because: <b>${e.message}</b>`, markup))
   );
+  // Authorization
   app.command('auth', ({ from, reply }) =>
     gapi.getClient(from.username)
       .then(client =>
         reply(`Send code from this <a href="${gapi.getLink(client)}">link</a> for authorization.`, markup))
       .catch(e => reply(`Can't do this, because: <b>${e.message}</b>`, markup))
   );
-  app.command('list', ({ from, reply }) =>
-    gapi.getClient(from.username)
-      .then(client => gapi.categories(client))
-      .then(values => reply(values.join(',\n')))
-      .catch(e => reply(`Can't do this, because: <b>${e.message}</b>`, markup))
-  );
-  app.command('help', ({ reply }) =>
-    reply('<b>Message format</b>:\nSUM CATEGORY; &lt;DESCRIPTION&gt; | &lt;DATE&gt;', markup)
-  );
-  app.hears(/([\d\.\+\-\*]+)\s([,а-яА-Я\w\s]+)(?:(?:;\s)([,а-яА-Я\w\s]*))?(?:(?:\s?\|\s?)(.*))?/, ({ match, from, reply }) =>
-    gapi.getClient(from.username)
+  // Adding data to spreadsheet
+  app.action(/set_category_(.+)\|(.+)\|(.+)/, async ({ match, from, reply, editMessageText, answerCbQuery }) => {
+    editMessageText(`Done! ${match[1]}: ${match[3]}; ${match[2]}`);
+    return gapi.getClient(from.username)
       .then(client => gapi.append(client, {
         amount: match[1],
         category: match[2],
         description: match[3],
-        date: match[4] || (new Date).toJSON().slice(0, 10),
+        date: (new Date).toJSON().slice(0, 10),
       }))
-      .then(msg => reply('Done!'))
+      .then(msg => answerCbQuery('Done!'))
       .catch(e => reply(`Can't do this, because: <b>${e.message}</b>`, markup))
+  });
+  // <Value> <Description>
+  app.hears(/([\d\.\+\-\*]+)\s([,а-яА-Я\w\s]+)/, ({ match, from, reply }) => {
+    const amount = match[1];
+    const description = match[2];
+    return gapi.getClient(from.username)
+      .then(client => gapi.categories(client))
+      .then(categories =>
+          reply(
+            `Set category for ${amount} ${description}`,
+            Markup.inlineKeyboard(
+              categories.map(cat => Markup.callbackButton(cat[0], `set_category_${amount}|${cat[0]}|${description}`)),
+              { columns: 2 }
+            ).extra()
+          )
+      )
+      .catch(e => reply(`Can't do this, because: <b>${e.message}</b>`, markup))
+  });
+  // Help
+  app.command('help', ({ reply }) =>
+    reply('<b>Message format</b>:\nSUM DESCRIPTION (for example, 1000 Sushi)', markup)
   );
+  // Auth key
   app.hears(/(.*)/, ({ match, from, reply }) =>
     gapi.getClient(from.username)
       .then(client => gapi.authorize(client, from.username, match[1]))
